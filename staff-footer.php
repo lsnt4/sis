ae607ca51e6a7004cedab595492855c4d5a4b3bf
lordBuddhika <32516393+lordBuddhika@users.noreply.github.com>
2018-08-28 13:19:15 +0530

    Footer color stripe added

--- staff-footer.php
+++ staff-footer.php
@@ -1,5 +1,6 @@
 			</div>
 		</div>
+		<div class="sll"><span class="sllm"></span><span class="sllo"></span><span class="sllg"></span></div>
 		<script src="assets/js/jquery-3.3.1.min.js"></script>
 		<script src="assets/js/bootstrap.js"></script>
 	</body>
