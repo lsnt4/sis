8eb1a2f07b47d95a4c7f7644d4708aed220da718
Pragadeesen <42860907+Pragadeesen@users.noreply.github.com>
2018-08-31 13:57:39 +0530

    Create Finance_Dashboard.php

---
+++ Finance_Dashboard.php
@@ -0,0 +1,63 @@
+<?php include_once 'staff-header.php'; ?>
+				<div class="col-md-10">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="card mb-4 p-2">
+								<img class="card-img-top p-4" src="assets/images/incomes.png" alt="Image">
+								<div class="card-body">
+									<a href="Finance_Add_Incomes.php" class="btn btn-lg btn-dark btn-block">Manage Incomes</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="card mb-4 p-2">
+								<img class="card-img-top p-4" src="assets/images/expenses.png" alt="Image">
+								<div class="card-body">
+									<a href="Finance_Add_Expenses.php" class="btn btn-lg btn-dark btn-block"> Manage Expenses </a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="card mb-4 p-2">
+								<img class="card-img-top p-4" src="assets/images/payslips.png" alt="Image">
+								<div class="card-body">
+									<a href="Finance_Add_Salary.php" class="btn btn-lg btn-dark btn-block"> Payroll </a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="card mb-4 p-2">
+								<img class="card-img-top p-4" src="assets/images/accounts.png" alt="Image">
+								<div class="card-body">
+									<a href="#" class="btn btn-lg btn-dark btn-block"> Accountings </a>
+								</div>
+							</div>
+						</div> 
+						<div class="col-md-3">
+							<div class="card mb-4 p-2">
+								<img class="card-img-top p-4" src="assets/images/overview.png" alt="Image">
+								<div class="card-body">
+									<a href="#" class="btn btn-lg btn-dark btn-block"> Finance Overview </a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="card mb-4 p-2">
+								<img class="card-img-top p-4" src="assets/images/reports.png" alt="Image">
+								<div class="card-body">
+									<a href="#" class="btn btn-lg btn-dark btn-block"> Financial Reports </a>
+								</div>
+							</div>
+						</div>
+                        <div class="col-md-3">
+							<div class="card mb-4 p-2">
+								<img class="card-img-top p-4" src="assets/images/accounts.png" alt="Image">
+								<div class="card-body">
+									<a href="#" class="btn btn-lg btn-dark btn-block"> Bank Accounts </a>
+								</div>
+							</div>
+						</div>
+                        </div>
+					</div>
+				</div>
+<?php include_once 'staff-ooter.php'; ?>
