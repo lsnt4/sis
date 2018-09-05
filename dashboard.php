8341114c46a469f13cd11a558665905cba05a57a
Buddhika <lsnt4@users.noreply.github.com>
2018-09-05 09:50:56 +0530

    Dashboard links updated

--- dashboard.php
+++ dashboard.php
@@ -107,16 +107,16 @@
 								<div class="card-body">
 									<div class="row">
 										<div class="col-md-3 mb-3">
-											<a href="staff-add.php" class="btn btn-lg btn-dark btn-block">Add Employee</a>
+											<a href="student-add.php" class="btn btn-lg btn-dark btn-block">Add Student</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-search.php" class="btn btn-lg btn-dark btn-block">Search Employees</a>
+											<a href="student-search.php" class="btn btn-lg btn-dark btn-block">Search Students</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-attendance.php" class="btn btn-lg btn-dark btn-block">Attendance</a>
+											<a href="student-attendance.php" class="btn btn-lg btn-dark btn-block">Attendance</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-departments.php" class="btn btn-lg btn-dark btn-block">Departments</a>
+											<a href="student-reports.php" class="btn btn-lg btn-dark btn-block">Reports</a>
 										</div>
 									</div>
 								</div>
@@ -132,16 +132,16 @@
 								<div class="card-body">
 									<div class="row">
 										<div class="col-md-3 mb-3">
-											<a href="staff-add.php" class="btn btn-lg btn-dark btn-block">Add Employee</a>
+											<a href="payment-add.php" class="btn btn-lg btn-dark btn-block">Add Payment</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-search.php" class="btn btn-lg btn-dark btn-block">Search Employees</a>
+											<a href="payment-search.php" class="btn btn-lg btn-dark btn-block">Search Payments</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-attendance.php" class="btn btn-lg btn-dark btn-block">Attendance</a>
+											<a href="payment-overview.php" class="btn btn-lg btn-dark btn-block">Overview</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-departments.php" class="btn btn-lg btn-dark btn-block">Departments</a>
+											<a href="payment-reports.php" class="btn btn-lg btn-dark btn-block">Reports</a>
 										</div>
 									</div>
 								</div>
@@ -157,16 +157,16 @@
 								<div class="card-body">
 									<div class="row">
 										<div class="col-md-3 mb-3">
-											<a href="staff-add.php" class="btn btn-lg btn-dark btn-block">Add Employee</a>
+											<a href="exam-add.php" class="btn btn-lg btn-dark btn-block">Add Exam</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-search.php" class="btn btn-lg btn-dark btn-block">Search Employees</a>
+											<a href="exam-search.php" class="btn btn-lg btn-dark btn-block">Search Exams</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-attendance.php" class="btn btn-lg btn-dark btn-block">Attendance</a>
+											<a href="exam-schedule.php" class="btn btn-lg btn-dark btn-block">Schedule</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-departments.php" class="btn btn-lg btn-dark btn-block">Departments</a>
+											<a href="exam-reports.php" class="btn btn-lg btn-dark btn-block">Reports</a>
 										</div>
 									</div>
 								</div>
@@ -188,10 +188,10 @@
 											<a href="course-search.php" class="btn btn-lg btn-dark btn-block">Search Courses</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="course-schedule.php" class="btn btn-lg btn-dark btn-block">Schedule Courses</a>
+											<a href="course-schedule.php" class="btn btn-lg btn-dark btn-block">Schedule</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-departments.php" class="btn btn-lg btn-dark btn-block">Departments</a>
+											<a href="course-reports.php" class="btn btn-lg btn-dark btn-block">Reports</a>
 										</div>
 									</div>
 								</div>
@@ -207,16 +207,16 @@
 								<div class="card-body">
 									<div class="row">
 										<div class="col-md-3 mb-3">
-											<a href="staff-add.php" class="btn btn-lg btn-dark btn-block">Add Employee</a>
+											<a href="finance-add.php" class="btn btn-lg btn-dark btn-block">Add Finance</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-search.php" class="btn btn-lg btn-dark btn-block">Search Employees</a>
+											<a href="finance-search.php" class="btn btn-lg btn-dark btn-block">Search Finance</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-attendance.php" class="btn btn-lg btn-dark btn-block">Attendance</a>
+											<a href="finance-approvals.php" class="btn btn-lg btn-dark btn-block">Approvals</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-departments.php" class="btn btn-lg btn-dark btn-block">Departments</a>
+											<a href="finance-reports.php" class="btn btn-lg btn-dark btn-block">Reports</a>
 										</div>
 									</div>
 								</div>
@@ -232,16 +232,16 @@
 								<div class="card-body">
 									<div class="row">
 										<div class="col-md-3 mb-3">
-											<a href="staff-add.php" class="btn btn-lg btn-dark btn-block">Add Employee</a>
+											<a href="library-book-add.php" class="btn btn-lg btn-dark btn-block">Add Book</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-search.php" class="btn btn-lg btn-dark btn-block">Search Employees</a>
+											<a href="library-book-search.php" class="btn btn-lg btn-dark btn-block">Search Books</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-attendance.php" class="btn btn-lg btn-dark btn-block">Attendance</a>
+											<a href="library-member-add.php" class="btn btn-lg btn-dark btn-block">Add Member</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-departments.php" class="btn btn-lg btn-dark btn-block">Departments</a>
+											<a href="library-member-search.php" class="btn btn-lg btn-dark btn-block">Search Members</a>
 										</div>
 									</div>
 								</div>
@@ -257,16 +257,16 @@
 								<div class="card-body">
 									<div class="row">
 										<div class="col-md-3 mb-3">
-											<a href="staff-add.php" class="btn btn-lg btn-dark btn-block">Add Employee</a>
+											<a href="resource-add.php" class="btn btn-lg btn-dark btn-block">Add Resource</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-search.php" class="btn btn-lg btn-dark btn-block">Search Employees</a>
+											<a href="resource-search.php" class="btn btn-lg btn-dark btn-block">Search Resources</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-attendance.php" class="btn btn-lg btn-dark btn-block">Attendance</a>
+											<a href="resource-overview.php" class="btn btn-lg btn-dark btn-block">Overview</a>
 										</div>
 										<div class="col-md-3 mb-3">
-											<a href="staff-departments.php" class="btn btn-lg btn-dark btn-block">Departments</a>
+											<a href="resource-reports.php" class="btn btn-lg btn-dark btn-block">Reports</a>
 										</div>
 									</div>
 								</div>
