fafeb9babcbcef5fa0bdf9718eef8cb00f10297e
Buddhika <lsnt4@users.noreply.github.com>
2018-09-07 01:15:26 +0530

    dashboard.php: completed

--- dashboard.php
+++ dashboard.php
@@ -4,22 +4,22 @@
 						<div class="col-md-12">
 							<div class="card mb-4 p-2">
 								<div class="row">
-									<div class="col-1"><span class="dashboard-avatar"><?php echo substr($session->get_session('fname'), 0,1); ?></span></div>
+									<div class="col-1"><span class="dashboard-avatar"><?php echo substr(get_session('fname'), 0,1); ?></span></div>
 									<div class="col-11">
-										<div class="dashboard-username"><?php echo $session->get_session('fname') . " " . $session->get_session('lname'); ?></div>
+										<div class="dashboard-username"><?php echo get_session('fname') . " " . get_session('lname'); ?></div>
 										<div class="dashboard-department">
-											<span class="badge badge-dark"><?php echo ucwords($session->get_session('user_role')); ?> Member</span>
-											<?php if ($session->get_session('user_role') == 'staff') { ?>
+											<span class="badge badge-dark"><?php echo ucwords(get_session('user_role')); ?> Member</span>
+											<?php if (get_session('user_role') == 'staff') { ?>
 												<span class="badge badge-pill">for</span>
-												<?php if($session->get_session('permission_students')) { ?><span class="badge badge-pill badge-secondary">student</span> <?php } ?>
-												<?php if($session->get_session('permission_staff')) { ?><span class="badge badge-pill badge-secondary">staff</span> <?php } ?>
-												<?php if($session->get_session('permission_payments')) { ?><span class="badge badge-pill badge-secondary">payments</span> <?php } ?>
-												<?php if($session->get_session('permission_exams')) { ?><span class="badge badge-pill badge-secondary">exams</span> <?php } ?>
-												<?php if($session->get_session('permission_courses')) { ?><span class="badge badge-pill badge-secondary">course</span> <?php } ?>
-												<?php if($session->get_session('permission_finance')) { ?><span class="badge badge-pill badge-secondary">finance</span> <?php } ?>
-												<?php if($session->get_session('permission_library')) { ?><span class="badge badge-pill badge-secondary">library</span> <?php } ?>
-												<?php if($session->get_session('permission_resources')) { ?><span class="badge badge-pill badge-secondary">resource</span> <?php } ?>
-												<?php if($session->get_session('permission_employees')) { ?><span class="badge badge-pill badge-secondary">employee</span> <?php } ?>
+												<?php if(get_session('permission_students')) { ?><span class="badge badge-pill badge-secondary">student</span> <?php } ?>
+												<?php if(get_session('permission_staff')) { ?><span class="badge badge-pill badge-secondary">staff</span> <?php } ?>
+												<?php if(get_session('permission_payments')) { ?><span class="badge badge-pill badge-secondary">payments</span> <?php } ?>
+												<?php if(get_session('permission_exams')) { ?><span class="badge badge-pill badge-secondary">exams</span> <?php } ?>
+												<?php if(get_session('permission_courses')) { ?><span class="badge badge-pill badge-secondary">course</span> <?php } ?>
+												<?php if(get_session('permission_finance')) { ?><span class="badge badge-pill badge-secondary">finance</span> <?php } ?>
+												<?php if(get_session('permission_library')) { ?><span class="badge badge-pill badge-secondary">library</span> <?php } ?>
+												<?php if(get_session('permission_resources')) { ?><span class="badge badge-pill badge-secondary">resource</span> <?php } ?>
+												<?php if(get_session('permission_employees')) { ?><span class="badge badge-pill badge-secondary">employee</span> <?php } ?>
 											<?php } ?>
 										</div>
 									</div>
@@ -74,7 +74,7 @@
 							</div>
 						</div>
 					</div>
-					<?php if($session->get_session('permission_staff')) { ?>
+					<?php if(get_session('permission_staff')) { ?>
 					<div class="row">
 						<div class="col-md-12">
 							<div class="card bg-light mb-3">
@@ -99,7 +99,7 @@
 						</div>
 					</div>
 					<?php } ?>
-					<?php if($session->get_session('permission_students')) { ?>
+					<?php if(get_session('permission_students')) { ?>
 					<div class="row">
 						<div class="col-md-12">
 							<div class="card bg-light mb-3">
@@ -124,7 +124,7 @@
 						</div>
 					</div>
 					<?php } ?>
-					<?php if($session->get_session('permission_payments')) { ?>
+					<?php if(get_session('permission_payments')) { ?>
 					<div class="row">
 						<div class="col-md-12">
 							<div class="card bg-light mb-3">
@@ -149,7 +149,7 @@
 						</div>
 					</div>
 					<?php } ?>
-					<?php if($session->get_session('permission_exams')) { ?>
+					<?php if(get_session('permission_exams')) { ?>
 					<div class="row">
 						<div class="col-md-12">
 							<div class="card bg-light mb-3">
@@ -174,7 +174,7 @@
 						</div>
 					</div>
 					<?php } ?>
-					<?php if($session->get_session('permission_courses')) { ?>
+					<?php if(get_session('permission_courses')) { ?>
 					<div class="row">
 						<div class="col-md-12">
 							<div class="card bg-light mb-3">
@@ -199,7 +199,7 @@
 						</div>
 					</div>
 					<?php } ?>
-					<?php if($session->get_session('permission_finance')) { ?>
+					<?php if(get_session('permission_finance')) { ?>
 					<div class="row">
 						<div class="col-md-12">
 							<div class="card bg-light mb-3">
@@ -224,7 +224,7 @@
 						</div>
 					</div>
 					<?php } ?>
-					<?php if($session->get_session('permission_library')) { ?>
+					<?php if(get_session('permission_library')) { ?>
 					<div class="row">
 						<div class="col-md-12">
 							<div class="card bg-light mb-3">
@@ -249,7 +249,7 @@
 						</div>
 					</div>
 					<?php } ?>
-					<?php if($session->get_session('permission_resources')) { ?>
+					<?php if(get_session('permission_resources')) { ?>
 					<div class="row">
 						<div class="col-md-12">
 							<div class="card bg-light mb-3">
