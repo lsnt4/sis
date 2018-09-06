691e6e17f66010c6c6a73b5ca0ac9d5ab1ea976c
Buddhika <lsnt4@users.noreply.github.com>
2018-09-07 01:11:40 +0530

    profile.php: completed

--- profile.php
+++ profile.php
@@ -1,9 +1,89 @@
 <?php include_once 'staff-header.php'; ?>
 <?php
-	$session = new SessionManager();
-	$userid = $session->get_session('userid');
-	$sm = new StaffManager();
-	$user = $sm->get_single_employee($userid)[0];
+	$userid = get_session('userid');
+
+	$user = array();
+	$result = $db_conn->query("
+		SELECT userid, fname, lname, email, mobile_no, address, dob, reg_date, salary, nic, gender
+		FROM users
+		WHERE userid='$userid'
+	");
+	if ($result->num_rows != 0) {
+		while($row = $result->fetch_assoc()) {
+
+			$departments = array();
+			$result_dept = $db_conn->query("
+				SELECT did, name, userid
+				FROM departments
+				LEFT JOIN user_departments ON departments.did=user_departments.department_id
+			");
+			if ($result_dept->num_rows != 0) {
+				while($row_dept = $result_dept->fetch_assoc()) {
+					array_push($departments, array(
+						'did' => $row_dept['did'],
+						'name' => $row_dept['name'],
+						'status' => ($row_dept['userid'] == $userid)
+					));
+				}
+			} else {
+				$departments = null;
+			}
+
+			array_push($user, array(
+				'userid' => $row['userid'],
+				'fname' => $row['fname'],
+				'lname' => $row['lname'],
+				'email' => $row['email'],
+				'mobile_no' => $row['mobile_no'],
+				'address' => $row['address'],
+				'dob' => $row['dob'],
+				'reg_date' => $row['reg_date'],
+				'salary' => $row['salary'],
+				'nic' => $row['nic'],
+				'gender' => $row['gender'],
+				'departments' => $departments,
+			));
+			break;
+		}
+	} else {
+		$user = null;
+	}
+	$user = $user[0];
+
+
+	if (isset($_POST['mobile_no']) && isset($_POST['address']) && isset($_POST['email'])) {
+		$mobile_no = $_POST['mobile_no'];
+		$address = $_POST['address'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+
+		$sql = "UPDATE users SET mobile_no='$mobile_no', address='$address', email='$email' WHERE userid='$userid'";
+		if ($db_conn->query($sql)) {
+			$status_progress = 1;
+		} else {
+			$status_progress = 0;
+			echo "Error updating record: " . $db_conn->error;
+		}
+
+		if ($password != '') {
+			$password = md5($password);
+			$sql = "UPDATE users SET password='$password' WHERE userid='$userid'";
+			if ($db_conn->query($sql)) {
+				$status_progress = 1;
+			} else {
+				$status_progress = 0;
+				echo "Error updating record: " . $db_conn->error;
+			}
+		}
+
+		if($status_progress) {
+			set_success_msg("<strong>Success!</strong> User has been successfully updated!");
+		} else {
+			set_error_msg("<strong>Failed!</strong> Something strange happened while trying to update!");
+		}
+
+		header('Location: profile.php');
+	}
 ?>
 				<div class="col-md-10">
 					<nav>
@@ -87,7 +167,7 @@
 									<div class="col-sm-10">
 										<div class="form-row">
 											<div class="col-md-6">
-												<input type="text" class="form-control" name="mobile" value="077123456">
+												<input type="text" class="form-control" name="mobile_no" value="<?php echo $user['mobile_no']; ?>">
 												<small class="form-text text-muted">Mobile</small>
 											</div>
 										</div>
@@ -110,7 +190,7 @@
 										<div class="form-row">
 											<div class="col-md-6">
 												<input type="email" class="form-control" name="email" value="<?php echo $user['email']; ?>">
-												<small class="form-text text-muted">Address</small>
+												<small class="form-text text-muted">Email</small>
 											</div>
 										</div>
 									</div>
@@ -130,7 +210,7 @@
 									<div class="col-sm-10">
 										<div class="form-row">
 											<div class="col-md-6">
-												<input type="password" class="form-control">
+												<input type="password" name="password" class="form-control">
 												<small class="form-text text-muted">Leave blank if no password change required</small>
 											</div>
 										</div>
